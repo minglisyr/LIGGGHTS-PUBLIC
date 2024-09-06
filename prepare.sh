89ffb7d2fe148a594d28aba4943a8882910c3c40
minglisyr <80295234+minglisyr@users.noreply.github.com>
2024-09-06 11:53:59 -0600

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,15 +1,14 @@
 cd $HOME
 sudo apt update
 sudo apt upgrade
-sudo apt-get install -y ffmpeg
 sudo apt-get install -y git
-
 git clone https://github.com/minglisyr/LIGGGHTS-PUBLIC.git
+sudo apt-get install -y ffmpeg openmpi-bin libopenmpi-dev g++ build-essential cmake libgl1-mesa-dev libglu1-mesa-dev paraview
 
-sudo apt-get install -y openmpi-bin libopenmpi-dev g++ build-essential cmake libgl1-mesa-dev libglu1-mesa-dev paraview
-sudo apt-get install -y libvtk9-dev paraview
+#sudo apt-get install -y libvtk9-dev paraview
+
 cd $HOME/LIGGGHTS-PUBLIC/src
-make auto
+make auto -j 12
 cd /usr/bin
 sudo ln -s $HOME/LIGGGHTS-PUBLIC/src/lmp_auto liggghts
 
