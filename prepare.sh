fe13ddf3b225413ed28388120b49befb8b910f3a
minglisyr <80295234+minglisyr@users.noreply.github.com>
2024-09-05 22:35:54 -0600

    Create prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,8 @@
+cd $HOME
+sudo apt update
+sudo apt upgrade
+sudo apt-get install -y ffmpeg
+sudo apt-get install -y git
+
+git clone https://github.com/minglisyr/LIGGGHTS-PUBLIC.git
+sudo apt-get install -y openmpi-bin libopenmpi-dev
